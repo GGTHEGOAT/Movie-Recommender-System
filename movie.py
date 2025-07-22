b298edcb6f2bd8632dc0691734a5bc6f6371b03c
GGTHEGOAT <77824316+GGTHEGOAT@users.noreply.github.com>
2025-07-22 09:01:19 +0545

    Add files via upload

--- movie.py
+++ movie.py
@@ -1,14 +1,33 @@
 import pickle
 import streamlit as st
 import requests
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+
+st.header('Movie Recommender System')
+
+# Load the movie data
+movies = pickle.load(open('movie_list.pkl','rb'))
+
+# Check if 'tags' column exists, if not, raise an error
+if 'tags' not in movies.columns:
+    st.error("The movie_list.pkl file does not contain the 'tags' column. Please preprocess your data to include it.")
+    st.stop()
+
+# Recompute similarity matrix
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+similarity = cosine_similarity(vectors)
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Poster+Available"
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -16,17 +35,10 @@ def recommend(movie):
     recommended_movie_names = []
     recommended_movie_posters = []
     for i in distances[1:6]:
-        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names,recommended_movie_posters
-
-
-st.header('Movie Recommender System')
-movies = pickle.load(open('movie_list.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
+    return recommended_movie_names, recommended_movie_posters
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
@@ -35,8 +47,7 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    # Changed st.beta_columns to st.columns
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
@@ -44,7 +55,6 @@ if st.button('Show Recommendation'):
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
